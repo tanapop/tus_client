64463ea4638b13be7b660776b73aaf78b88cf385
Diswarin Saiphanas <tanapop@hotmail.com>
2021-08-21 00:20:41 +0700

    update

--- lib/src/client.dart
+++ lib/src/client.dart
@@ -20,8 +20,8 @@ class TusClient {
   /// Storage used to save and retrieve upload URLs by its fingerprint.
   final TusStore? store;
   
-  final String? name;
-  final String? description;
+  final String? name = "";
+  final String? description = "";
 
   final XFile file;
 
